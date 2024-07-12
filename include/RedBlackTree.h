f4ff08c0ebe6571cdf5123ad80b0ab7eefa2d6ca
kotiko10 <koti1.nebieridze@gmail.com>
2024-07-12 21:15:05 +0200

    starting with red balck trees

--- include/RedBlackTree.h
+++ include/RedBlackTree.h
@@ -1,8 +1,11 @@
+#include<cstdint>
+
 template<typename T>
 class RB_Tree{
 
 private:
 
+    uint32_t node_count; // number of nodes in the tree
     struct RedBlackTree
     {
         T node;
@@ -12,4 +15,12 @@ private:
         bool color;
         BinarySearchTree (T val) : node (val), left(nullptr), right(nullptr), color(0) {}
     };
+
+    void rotate_left();
+    void rotate_right();
+
+public:
+    void add_node();
+    void delete_node();
+
 };
